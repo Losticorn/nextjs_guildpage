b727367083bea4285a0f8bf8d1b8619f2543b17b
Jaro <jaroslavfilo95@gmail.com>
2024-08-28 14:18:18 +0200

    replaced env

--- app/manage/applications/page.js
+++ app/manage/applications/page.js
@@ -1,34 +1,3 @@
-import mongoose from "mongoose";
-import ApplicationList from "../../components/Application/ApplicationList";
-import classes from "../../styles/ManageApplications.module.css";
-
-export async function getApplications() {
-  const client = await mongoose.connect(process.env.MONGO_URI);
-
-  const db = client.db();
-
-  const applicationsCollection = db.collection("application");
-
-  const applications = await applicationsCollection.find().toArray();
-
-  client.close();
-
-  return {
-    props: {
-      applications: applications.map(({ _id, main = "", ...rest }) => ({
-        id: _id.toString(),
-        main,
-        ...rest,
-      })),
-    },
-  };
-}
-
-export default function ManageApplications(props) {
-  return (
-    <main className={classes.mainapplications}>
-      <h1>Applications</h1>
-      <ApplicationList applications={props.applications} />
-    </main>
-  );
+export default function Applications() {
+  return <h1>Applications</h1>;
 }
